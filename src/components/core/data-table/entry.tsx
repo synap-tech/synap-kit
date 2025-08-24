2ccaa960eaf3b40341ee6a0080310976732853bc
alifbinhossain <alifahmed47@gmail.com>
2025-08-24 18:20:57 +0600

    feat: add DataTableEntry component to encapsulate TableProvider for data tables

---
+++ src/components/core/data-table/entry.tsx
@@ -0,0 +1,26 @@
+import { TableProvider } from '@/providers';
+import type { ITableProviderProps } from '@/providers/table-provider';
+
+const DataTableEntry = <TData, TValue>(
+  props: Pick<
+    ITableProviderProps<TData, TValue>,
+    | 'title'
+    | 'columns'
+    | 'data'
+    | 'toolbarOptions'
+    | 'defaultVisibleColumns'
+    | 'otherToolBarComponents'
+    | 'handleRefetch'
+    | 'enableDefaultColumns'
+    | 'isLoading'
+  > & { children?: React.ReactNode }
+) => {
+  const { children, ...rest } = props;
+  return (
+    <TableProvider isEntry enableRowSelection={false} {...rest}>
+      {children}
+    </TableProvider>
+  );
+};
+
+export default DataTableEntry;
