79cbf96dbe9abe70c8d289dd1fa214350a8f9b66
alifbinhossain <alifahmed47@gmail.com>
2025-08-26 17:13:23 +0600

    feat: add dynamic field types and interface for dynamic fields component

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,7 @@
 import type { UseMutationResult } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
+//Dynamic Field Types
+import type { FieldArrayWithId, UseFormReturn } from 'react-hook-form';
 import type { RouteObject } from 'react-router-dom';
 
 export type IToast = {
@@ -179,4 +181,138 @@ export interface IDefaultAddOrUpdateProps {
     },
     any
   >;
+}
+
+type FieldReadonly = {
+  type: 'readOnly';
+};
+type FieldCustom = {
+  type: 'custom';
+  component: (index: number) => React.ReactNode;
+};
+
+type FieldText = {
+  type: 'text';
+  // inputType?: 'text' | 'number';
+  placeholder?: string;
+  disabled?: boolean;
+};
+type FieldTextArea = {
+  type: 'textarea';
+  placeholder?: string;
+  disabled?: boolean;
+};
+type FieldNumber = {
+  type: 'number';
+  placeholder?: string;
+  disabled?: boolean;
+};
+type FieldCheckBox = {
+  type: 'checkBox';
+};
+type FieldSelect = {
+  type: 'select';
+  placeholder?: string;
+  options: IFormSelectOption[];
+  excludeOptions?: string[];
+  unique?: boolean;
+  disabled?: boolean;
+  onChange?: (option?: any, field?: any) => void;
+};
+
+type FieldRadio = {
+  type: 'radio';
+  placeholder?: string;
+  options: IFormSelectOption[];
+  disabled?: boolean;
+  onChange?: (option?: any, field?: any) => void;
+};
+type FieldJoinInputUnit = {
+  type: 'join-input-unit';
+  placeholder?: string;
+  unit: (index: number) => string;
+  disabled?: boolean;
+  inputType?: string;
+};
+
+type FieldImage = {
+  type: 'image';
+  placeholder?: string;
+  isUpdate?: boolean;
+  baseUrl: string;
+};
+type FieldFile = {
+  type: 'file';
+  placeholder?: string;
+  isUpdate?: boolean;
+  baseUrl: string;
+};
+
+type FieldCheckbox = {
+  type: 'checkbox';
+  placeholder?: string;
+  isUpdate?: boolean;
+  disabled?: boolean;
+};
+
+type FieldDate = {
+  type: 'date';
+  placeholder?: string;
+  isUpdate?: boolean;
+  disabled?: boolean;
+};
+
+type FieldMultiSelect = {
+  type: 'multiSelect';
+  placeholder?: string;
+  options: IFormSelectOption[];
+};
+
+type FieldSelectCreate = {
+  type: 'select-create';
+  placeholder?: string;
+  options: IFormSelectOption[];
+};
+
+export type FieldDef = {
+  header: string;
+  accessorKey: string;
+  className?: string;
+  isLoading?: boolean;
+  hidden?: boolean;
+  width?: string | number;
+  maxWidth?: string | number;
+  minWidth?: string | number;
+  disabled?: boolean;
+} & (
+  | FieldText
+  | FieldNumber
+  | FieldSelect
+  | FieldReadonly
+  | FieldCustom
+  | FieldJoinInputUnit
+  | FieldTextArea
+  | FieldCheckBox
+  | FieldMultiSelect
+  | FieldSelectCreate
+  | FieldImage
+  | FieldCheckbox
+  | FieldDate
+  | FieldFile
+  | FieldRadio
+);
+
+export interface DynamicFieldsProps {
+  title: string | React.ReactNode;
+  form: UseFormReturn<any>;
+  fieldName: string;
+  fieldDefs: FieldDef[];
+  extraHeader?: React.ReactNode;
+  handleAdd?: () => void;
+  fields: FieldArrayWithId<any>[];
+  viewAs?: 'default' | 'spreadsheet' | 'kanban';
+  containerClassName?: string;
+  className?: string;
+  children?: React.ReactNode;
+  startIndex?: number;
 }
