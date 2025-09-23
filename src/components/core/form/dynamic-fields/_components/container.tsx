54e5cf26627f777240b447dcf2217947560f37f1
alifbinhossain <alifahmed47@gmail.com>
2025-09-23 19:14:42 +0600

    refactor: simplify DynamicFieldContainer layout and improve header structure

--- src/components/core/form/dynamic-fields/_components/container.tsx
+++ src/components/core/form/dynamic-fields/_components/container.tsx
@@ -28,30 +28,28 @@ const DynamicFieldContainer: React.FC<IProps> = ({
 }) => {
   return (
     <div className='overflow-hidden rounded-md shadow-sm bg-base'>
-      <SectionHeader extraHeader={extraHeader}>{title}</SectionHeader>
-      {/* <div className='flex items-center justify-between bg-primary py-2 pl-4 pr-2'>
-        <h3 className='text-lg font-medium text-primary-foreground'>
-          {title || 'Dynamic Fields'}
-        </h3>
-
-        <div className='flex items-center gap-4'>
-          {extraHeader}
-          {handleAdd && (
-            <Button
-              onClick={handleAdd}
-              type='button'
-              variant={'gradient-accent'}
-              size={'xs'}
-              className='gap-1 rounded'
-            >
-              <Plus className='size-4' />
-              New
-            </Button>
-          )}
-          {extraButton}
-        </div>
-      </div> */}
-
+      <SectionHeader
+        extraHeader={
+          <div className='flex items-center gap-4'>
+            {extraHeader && extraHeader}
+            {handleAdd && (
+              <Button
+                onClick={handleAdd}
+                type='button'
+                variant={'accent'}
+                size={'xs'}
+                className='gap-1 rounded-md'
+              >
+                <Plus className='size-4' />
+                New
+              </Button>
+            )}
+            {extraButton && extraButton}
+          </div>
+        }
+      >
+        {title}
+      </SectionHeader>
       <div className={cn('px-5 pb-4 ', containerClassName)}>{children}</div>
     </div>
   );
