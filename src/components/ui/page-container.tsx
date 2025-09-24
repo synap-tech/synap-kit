5faa9dcfe5b6965a9ea6ef535624d55528d8722d
alifbinhossain <alifahmed47@gmail.com>
2025-09-24 15:48:53 +0600

    feat: add PageContainer component for consistent layout and styling

---
+++ src/components/ui/page-container.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { cn } from '@/lib/utils';
+
+const PageContainer: React.FC<{
+  children: React.ReactNode;
+  className?: string;
+}> = ({ children, className }) => {
+  return (
+    <div className={cn('border bg-base p-4 rounded-md', className)}>
+      {children}
+    </div>
+  );
+};
+
+export default PageContainer;
