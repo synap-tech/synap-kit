fc560aded9cb2128f1937ee05d820c18ead734e8
alifbinhossain <alifahmed47@gmail.com>
2025-09-25 03:49:59 +0600

    feat: update TestForm layout to enhance user experience with dynamic fields and kanban view

--- src/pages/test/form.tsx
+++ src/pages/test/form.tsx
@@ -52,80 +52,78 @@ const TestForm = () => {
   const handleRemove = (index: number) => {};
   const handleAdd = (index: number) => {};
   return (
-    <SchemaProvider schema={schema}>
-      <Form {...form}>
-        <div className='space-y-4'>
-          <div className='grid grid-cols-3 gap-4'>
-            <div className='col-span-1'>
-              <CoreForm.Section
-                title='Information'
-                className='lg:grid-cols-1 gap-4'
-              >
-                <FormField
-                  control={form.control}
-                  name='name'
-                  render={(props) => <CoreForm.Input {...props} />}
-                />
-                <FormField
-                  control={form.control}
-                  name='customer_uuid'
-                  render={(props) => (
-                    <CoreForm.ReactSelect
-                      menuPortalTarget={document.body}
-                      label='Customer'
-                      placeholder='Select Customer'
-                      options={[]}
-                      {...props}
-                    />
-                  )}
-                />
-              </CoreForm.Section>
-            </div>
-
-            <div className='col-span-2'>
-              <CoreForm.DynamicFields
-                viewAs='kanban'
-                title='Ready For Delivery'
-                form={form as any}
-                fieldName='new_challan_entries'
-                fieldDefs={useGenerateFieldDefs({
-                  entry: 'new_challan_entries',
-                  remove: handleRemove,
-                  add: handleAdd,
-                  watch: form.watch,
-                })}
-                fields={newFields}
-                handleAdd={() => {
-                  alert('Add new entry');
-                }}
+    <Form {...form}>
+      <div className='space-y-4'>
+        <div className='grid grid-cols-3 gap-4'>
+          <div className='col-span-1'>
+            <CoreForm.Section
+              title='Information'
+              className='lg:grid-cols-1 gap-4'
+            >
+              <FormField
+                control={form.control}
+                name='name'
+                render={(props) => <CoreForm.Input {...props} />}
               />
-            </div>
+              <FormField
+                control={form.control}
+                name='customer_uuid'
+                render={(props) => (
+                  <CoreForm.ReactSelect
+                    menuPortalTarget={document.body}
+                    label='Customer'
+                    placeholder='Select Customer'
+                    options={[]}
+                    {...props}
+                  />
+                )}
+              />
+            </CoreForm.Section>
           </div>
 
-          <CoreForm.DynamicFields
-            title='Challan Entry'
-            form={form as any}
-            fieldName='challan_entries'
-            fieldDefs={useGenerateFieldDefs({
-              entry: 'challan_entries',
-              remove: handleRemove,
-              add: handleAdd,
-              watch: form.watch,
-            })}
-            fields={fields}
-          >
-            <tr>
-              <td className='border-t text-right font-semibold' colSpan={4}>
-                Grand Total:
-              </td>
-
-              <td className='border-t px-3 py-2'>{200}</td>
-              <td className='border-t px-3 py-2'></td>
-            </tr>
-          </CoreForm.DynamicFields>
+          <div className='col-span-2'>
+            <CoreForm.DynamicFields
+              viewAs='kanban'
+              title='Ready For Delivery'
+              form={form as any}
+              fieldName='new_challan_entries'
+              fieldDefs={useGenerateFieldDefs({
+                entry: 'new_challan_entries',
+                remove: handleRemove,
+                add: handleAdd,
+                watch: form.watch,
+              })}
+              fields={newFields}
+              handleAdd={() => {
+                alert('Add new entry');
+              }}
+            />
+          </div>
         </div>
-      </Form>
-    </SchemaProvider>
+
+        <CoreForm.DynamicFields
+          title='Challan Entry'
+          form={form as any}
+          fieldName='challan_entries'
+          fieldDefs={useGenerateFieldDefs({
+            entry: 'challan_entries',
+            remove: handleRemove,
+            add: handleAdd,
+            watch: form.watch,
+          })}
+          fields={fields}
+        >
+          <tr>
+            <td className='border-t text-right font-semibold' colSpan={4}>
+              Grand Total:
+            </td>
+
+            <td className='border-t px-3 py-2'>{200}</td>
+            <td className='border-t px-3 py-2'></td>
+          </tr>
+        </CoreForm.DynamicFields>
+      </div>
+    </Form>
   );
 };
 
