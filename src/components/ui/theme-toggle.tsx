65aec56bd5e35be54aa0e5d68040cdd39c3a242f
alifbinhossain <alifahmed47@gmail.com>
2025-10-13 21:12:32 +0600

    style: :art: redesigned theme toggle

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -1,33 +1,42 @@
-import { MoonIcon, SunIcon } from 'lucide-react';
+import { useCallback, useEffect, useState } from 'react';
+
+import type { Theme } from '@/providers/theme-provider';
 
 import useTheme from '@/hooks/useTheme';
 
-import { Button } from '@/components/ui/button';
-
-import { cn } from '@/lib/utils';
+import {
+  ThemeToggleButton,
+  useThemeTransition,
+} from '../buttons/theme-toggle-button';
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme();
 
+  const { startTransition } = useThemeTransition();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  const handleThemeToggle = useCallback(() => {
+    const newMode: Theme = theme === 'dark' ? 'light' : 'dark';
+
+    startTransition(() => {
+      setTheme(newMode);
+    });
+  }, [setTheme, startTransition, theme]);
+
+  const currentTheme = theme === 'dark' ? 'dark' : 'light';
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
-    <Button
-      variant='outline'
-      size='icon-lg'
-      onClick={() => {
-        setTheme(theme === 'dark' ? 'light' : 'dark');
-      }}
-      aria-label='Toggle dark mode'
-      className={cn(
-        theme === 'dark'
-          ? 'border-sky-600 text-sky-600! hover:bg-sky-600/10 focus-visible:border-sky-600 focus-visible:ring-sky-600/20 dark:border-sky-400 dark:text-sky-400! dark:hover:bg-sky-400/10 dark:focus-visible:border-sky-400 dark:focus-visible:ring-sky-400/40'
-          : 'border-amber-600 text-amber-600! hover:bg-amber-600/10 focus-visible:border-amber-600 focus-visible:ring-amber-600/20 dark:border-amber-400 dark:text-amber-400! dark:hover:bg-amber-400/10 dark:focus-visible:border-amber-400 dark:focus-visible:ring-amber-400/40'
-      )}
-    >
-      {theme === 'dark' ? (
-        <MoonIcon className='size-5' />
-      ) : (
-        <SunIcon className='size-5' />
-      )}
-    </Button>
+    <ThemeToggleButton
+      theme={currentTheme}
+      onClick={handleThemeToggle}
+      variant='circle'
+      start='top-right'
+    />
   );
 }
